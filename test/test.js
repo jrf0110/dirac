33b4c244c7144336c7f9d8669b45dd2a32d7688d
John Fawcett <jrf0110@gmail.com>
2013-08-22 14:41:43 -0500

    added register test

--- test/test.js
+++ test/test.js
@@ -10,33 +10,66 @@ var dbConfig = {
 var connString = 'postgres://' + dbConfig.host + ':' + dbConfig.port + '/' + dbConfig.database;
 
 describe ('Root API', function(){
-  it ('should initialize with a connStr', function(){
-    dirac.init( connString );
-    assert( dirac.options.connString == connString );
-    assert( dirac.dals.dirac_schemas instanceof dirac.DAL );
+
+  describe ('dirac.init', function(){
+
+    it ('should initialize with a connStr', function(){
+      dirac.init( connString );
+      assert( dirac.options.connString == connString );
+      assert( dirac.dals.dirac_schemas instanceof dirac.DAL );
+    });
+
+    it ('should initialize with options', function(){
+      dirac.init( dbConfig );
+      assert( dirac.options.connString == connString );
+      assert( dirac.dals.dirac_schemas instanceof dirac.DAL );
+    });
+
+    it ('should initialize with default options', function(){
+      dirac.init({ database: dbConfig.database });
+      assert( dirac.options.connString == connString );
+      assert( dirac.dals.dirac_schemas instanceof dirac.DAL );
+    });
+
+    it ('should throw an error because missing connString', function(){
+      assert.throws( function(){
+        dirac.init();
+      }, Error)
+    });
+
+    it ('should throw an error because missing host', function(){
+      assert.throws( function(){
+        dirac.init({});
+      }, Error)
+    });
+
   });
 
-  it ('should initialize with options', function(){
-    dirac.init( dbConfig );
-    assert( dirac.options.connString == connString );
-    assert( dirac.dals.dirac_schemas instanceof dirac.DAL );
-  });
+  describe ('dirac.register', function(){
 
-  it ('should initialize with default options', function(){
-    dirac.init({ database: dbConfig.database });
-    assert( dirac.options.connString == connString );
-    assert( dirac.dals.dirac_schemas instanceof dirac.DAL );
-  });
+    it ('should register a new table', function(){
+      dirac.register({
+        name: 'users'
+      , schema: {
+          id: {
+            type: 'serial'
+          , primaryKey: true
+          }
+        , name: { type: 'text' }
+        }
+      });
 
-  it ('should throw an error because missing connString', function(){
-    assert.throws( function(){
-      dirac.init();
-    }, Error)
-  });
+      assert( dirac.dals.users instanceof dirac.DAL );
+      dirac.unregister( 'users' );
+    });
 
-  it ('should throw an error because missing host', function(){
-    assert.throws( function(){
-      dirac.init({});
-    }, Error)
+    it ('should throw an error because the schema is missing', function(){
+      assert.throws( function(){
+        dirac.register({
+          name: 'users'
+        });
+      });
+    });
+
   });
 });
