ce07966cd1d2bb605d79dfe403b5590c34b352f1
John Fawcett <jrf0110@gmail.com>
2013-12-22 11:03:58 -0600

    move middleware tests to a single middleware test file, running tests in order works

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var dirac = module.exports = require('./lib/dirac');
 var diracSchema = require('./lib/dirac-table');
 
 // Middleware
-dirac.tableRef = require('./middleware/table-ref');
-dirac.castToJSON = require('./middleware/cast-to-json');
+dirac.tableRef    = require('./middleware/table-ref');
+dirac.castToJSON  = require('./middleware/cast-to-json');
+dirac.embeds      = require('./middleware/embeds');
